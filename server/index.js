94c44a08d6aedab47876ccadb4a9b363a6d3f9f4
Bhaveshanjana <bhaveshanjana58@gmail.com>
2025-05-08 22:34:40 +0530

    refactor: improve AI prompt structure

--- server/index.js
+++ server/index.js
@@ -23,14 +23,15 @@ app.post("/api/plan-trip", async (req, res) => {
   try {
     const prompt = `
       Generate Travel plan for location ${destination} for ${travelers} person(s) 
-      for ${days} days with a budget of $${budget}.give me hotels option's list with hotel name, hotel address, price, rating and description also suggest itinerary with place name,place details,ticket pricing,rating, time travel of each location for ${days} with each day plan with best time to visit in JSON formate
+      for ${days} with a budget of $${budget}.give me hotels option's list with hotel's name, hotel address, price, rating and description also suggest itinerary with place name,place details,ticket pricing,rating, time travel of each location for ${days} with each day plan with best time to visit.Give me a trip plan in the following Respond ONLY with a raw JSON object. Do not wrap it in markdown or code block formatting also give exactly — always include trip_details as an object with fields: location, duration, travelers, budget. hotel_options as an array in that array an object with fields: hotel_name, hotel_address, price_per_night, rating, description, itinerary as an array in that array an object with fields: day_1 as per user entered days,theme, plan as an array in that array an object with fields:place_name,place_details,ticket_pricing,time_travel,best_time_to_visit,rating,notes
     `;
 
     const result = await genAI.models.generateContent({
       model: "gemini-2.0-flash",
       contents: prompt,
     });
-    const response = await result.candidates[0].content.parts[0].text;
+    const response = result.candidates[0].content.parts[0].text;
+    res.status(200).json(response);
     console.log(response);
   } catch (err) {
     console.error("Gemini error:", err);
